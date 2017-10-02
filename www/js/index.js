023d59af103acb0d824e9b949c0585748f7280b7
Einstainer <rana2_15@uni.worc.ac.uk>
2017-10-02 15:42:19 +0100

    Simplifying and explaining the code
    Removed excess code by teachers behest and commented most of it, added
    prints for the user of model and  platform

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,14 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+document.addEventListener('deviceready', onDeviceReady, false); //creates an eventlistener for deviceready which comes from concorde code called from html 
+ 
+//var frog = 2; assumes the type as it doesn't define strings, ints etc separately.
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-    }
-};
+function onDeviceReady()  {
+       alert("Device Ready"); //Alert = printf on javascript
+    frog = device.platform; //creates a variable on it's own assuming you need one, but exists outside function
+    var cat = device.model; //creates a variable within the function
+    alert(frog + 3); //prints everything in string as it assumes, one being a string, then both have to be
+    alert (cat);
+    
+    
+}
